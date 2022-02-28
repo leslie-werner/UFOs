c2c333471a5a348b31084dc46190dc5b28ffaf9d
Leslie Werner <lesliewerner@Leslies-MBP.lan>
2022-02-28 16:52:19 -0500

    Edited

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,30 @@ function buildTable(data) {
       );
     });
   }
+
+//handling what to do after an input is given, such as filtering the table by date.
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+
+//  D3 to execute our handleClick() function when the button with an id of filter-btn is clicked.
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+// Build the table when the page loads
+buildTable(tableData);
